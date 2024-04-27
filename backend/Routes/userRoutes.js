41a10fb46c46975ff99b65c44c55fae6c9b98904
Jui1 <juimandal31@gmail.com>
2024-04-28 02:30:02 +0530

    Your commit message

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { Login, Register } from "../controllers/userControlers.js";
+import { Login, Register, logout } from "../controllers/userControlers.js";
 
 const router = express.Router();
 
 router.route("/register").post(Register);
 router.route("/login").post(Login);
+router.route("/logout").get(logout);
+
+
 export default router;
--- backend/controllers/userControlers.js
+++ backend/controllers/userControlers.js
@@ -47,19 +47,19 @@ export const Login =  async(req,res) =>{
         const {email, password} =  req.body;
         if(!email || !password){
             return res.status(401).json({
-                message: "Incorrect email and password",
+                message: "All fields are required.",
                 success: false
             });
         }
-        const user = await findOne({email});
+        const user = await  User.findOne({email});
         if(!user){
             return res.status(401).json({
-                message :"User does not exist with this email",
-                success :false
+                message: "Incorrect email or password",
+                success: false
             })
         }
 
-        const isMatch = await bcryptjs.compare(user.password,  password );
+        const isMatch = await bcryptjs.compare( password,user.password  );
         if(!isMatch){
             return res.status(401).json({
                 message:"Incorrent email or password",
@@ -69,12 +69,20 @@ export const Login =  async(req,res) =>{
         const tokenData = {
           userId:user._id  
         }
-        const token = await jwt.sign({tokenData},process.env.TOKENSECRET,{expiresIn:"1d" , httpOnly :true}).json({
-           
+        const token = await jwt.sign({tokenData},process.env.TOKENSECRET,{expiresIn:"1d"}); 
+           return res.status(201).cookie("token", token,{expiresIn: "1d", httpOnly: true }).json({
             message:`Welcome back ${user.name}`,
+            user,
             success:true
         })
     } catch (error) {
         console.log(error)
     }
+}
+
+export const logout = (req, res) => {
+    return res.cookie("token", "", { expiresIn: new Date(Date.now()) }).json({
+        message: "user logged out successfully.",
+        success: true
+    })
 }
