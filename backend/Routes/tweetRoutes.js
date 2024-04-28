6b91d2dc27e6fbe6a6c4a3dd045786bd0eb97f79
Jui1 <juimandal31@gmail.com>
2024-04-28 17:22:41 +0530

    first commit

--- backend/Routes/tweetRoutes.js
+++ backend/Routes/tweetRoutes.js
@@ -1,10 +1,14 @@
 import express from "express" ;
-import { createTweet, deeleteTweet } from "../controllers/TweetController.js";
+import { createTweet,  deleteTweet,likeOrDislike } from "../controllers/TweetController.js";
 import isAuthenticated from "../config/auth.js"
 
 const router = express.Router();
 
 router.route("/create").post(isAuthenticated,createTweet)
-router.route("/delete/:id").delete(isAuthenticated, deeleteTweet );
+router.route("/delete/:id").delete(isAuthenticated, deleteTweet);
+
+router.route("/like/:id").put(isAuthenticated,likeOrDislike);
+
+
 
 export default router;
--- backend/controllers/TweetController.js
+++ backend/controllers/TweetController.js
@@ -5,7 +5,7 @@
     try{
   const {description, id} = req.body;
      if(!description || !id){
-        return res.ststud(401).json({
+        return res.status(401).json({
             message:"Fields are required",
             success:false
         })
@@ -23,15 +23,39 @@
     }
   }
 
-  export const deleteTweet = async (req,res) => {
+  export const deleteTweet = async (req, res) => {
     try {
-        const {id}  = req.params;
+        const { id }  = req.params;
         await Tweet.findByIdAndDelete(id);
         return res.status(200).json({
-            message:"Tweet deleted successfully.",
-            success:true
-        })
+            message: "Tweet deleted successfully.",
+            success: true
+        });
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+
+export const likeOrDislike = async (req,res) => {
+    try {
+        const loggedInUserId = req.body.id;
+        const tweetId = req.params.id;
+        const tweet = await Tweet.findById(tweetId);
+        if(tweet.like.includes(loggedInUserId)){
+          
+            await Tweet.findByIdAndUpdate(tweetId,{$pull:{like:loggedInUserId}});
+            return res.status(200).json({
+                message:"User disliked your tweet."
+            })
+        }else{
+           
+            await Tweet.findByIdAndUpdate(tweetId, {$push:{like:loggedInUserId}});
+            return res.status(200).json({
+                message:"User liked your tweet."
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
